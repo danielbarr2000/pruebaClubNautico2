9deea9dc57331863cccc59585b6190cf314253f9
danielbarr2000 <danielbarr2000@gmail.com>
2023-06-14 13:17:11 +0200

    añadida ventana tablas basica

--- src/app/barcos/barcos/barcos.component.ts
+++ src/app/barcos/barcos/barcos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BarcosService } from './service/barcos.service';
 
 @Component({
   selector: 'app-barcos',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class BarcosComponent {
 
+  nombre!: any;
+  articulos: any;
+  constructor(private barcosService: BarcosService, public router: Router) { }
+
+  ngOnInit(): void {
+
+    this.getUsuario();
+    this.barcosService.acceder().subscribe({
+      next: (resp) => {
+        this.articulos = resp;
+        console.log(this.articulos);
+
+      }, error: (err) => {
+        console.log(err);
+
+      }
+    })
+  }
+
+  getUsuario() {
+    if (localStorage.getItem('nombre') != '') {
+      this.nombre = localStorage.getItem('nombre') as string
+      console.log(this.nombre);
+    }
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem('nombre')
+    this.router.navigate(['/']);
+  }
+
+  borrar(id: String) {
+    this.barcosService.borrar(id).subscribe();
+    console.log("Borrado socio: " + id);
+
+    setTimeout(() => {
+      console.log('sleep');
+      this.router.navigate(['barcos']);
+    }, 1000);
+  }
 }
